97665c7e780b9e26cc179bcc6db4353195e74f94
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-10-30 23:19:13 +0500

    New Tasks Form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   # We have to build our first action to display all the tasks so # we're gonna fetch all the tasks from the database.
   def index
-   @tasks = Task.all.limit(25)
+    @tasks = Task.all.limit(25)
+    @task = Task.new
   end
 end
