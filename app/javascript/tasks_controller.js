ba6b5fe48aaf37aff4cbfe0ebfbc69a54d33f92b
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-01 15:37:25 +0500

    Using Tasks Stimulus Controller

--- app/javascript/tasks_controller.js
+++ app/javascript/tasks_controller.js
@@ -1,8 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
-    connect()
-        console.log(this.element)
-    }
     toggle(e) {
         const id = e.target.dataset.id
         const csrfToken = document.querySelector("[name='csrf-token']").content
@@ -22,3 +19,4 @@ export default class extends Controller {
              alert(data.message)
            })
     }
+}
